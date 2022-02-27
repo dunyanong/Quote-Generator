81d4f2785503d96be74ed483756186d6ff286181
dun_yan_ <dunyanong2004@gmail.com>
2022-02-27 14:20:51 +0800

    first commit

---
+++ QG.js
@@ -0,0 +1,28 @@
+let sentences = ["Clean Up Your Room when you feel Shitty",
+  "Even the most beautiful flower grow through dirt",
+  "Start With Why -- Simon Sinek",
+  "Live as if you were to die tomorrow. Learn as if you were to live forever. -- Gandhi",
+  "Control your own destiny or someone else will. -- Jack Welch",
+  'True friends stab you in the front.',
+  "Your time is limited, so don't waste it living someone else's life. -- Steve Jobs." ,
+  "You dont need to have a 100-person company to develop that idea. -— Larry Page",
+]
+
+let button = document.querySelector('.glow-on-hover');
+let quote = document.getElementById('quote');
+
+function fortuneSelector(){
+  let randomQuotes = Math.floor(Math.random() * sentences.length);
+  return sentences[randomQuotes];
+}
+
+function showFortune(){
+  quote.innerHTML = fortuneSelector();
+  button.innerHTML = "CLick me again";
+  button.style.cursor = "default";
+
+  //add your code here
+
+}
+
+button.addEventListener('click', showFortune);
